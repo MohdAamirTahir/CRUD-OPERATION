4015101b1131882f9af53310ef3cd3dee36bb853
Aamir Khan <zunzd.khan@gmail.com>
2023-09-20 11:26:36 +0530

    Changed db connection url

--- db/connectdb.js
+++ db/connectdb.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const connectdb = mongoose.connect("mongodb://127.0.0.1/CRUDDATABASE")
+const connectdb = mongoose.connect("mongodb+srv://aamir:jtXrWaePu2aZx4Qz@cluster0.ktyehk8.mongodb.net/")
 .then((req,res)=>console.log("Now You Are Connected Databse"))
 .catch((req,res)=>console.log("Something Went Wrong"))
 
